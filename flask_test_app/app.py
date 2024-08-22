e833740bf7832f355d14a09c36a940a721953758
Nicholas Day <71455485+NicholasDay1992@users.noreply.github.com>
2024-08-22 20:55:30 +0100

    started sorting page

--- flask_test_app/app.py
+++ flask_test_app/app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template
 #from data_processing import get_stack
 from data_set import get_data
 from search_data import linear_search, binary_s
+from sort_data import bubble_sort, conduct_merge_sort
 
 app = Flask(__name__)
 
@@ -21,6 +22,13 @@ def search():
     linear = linear_search()
     binary = binary_s()
     return render_template('search.html', linear=linear, binary=binary)
+
+@app.route('/sort')
+def sort():
+    bubble = bubble_search()
+    merge = conduct_merge_sort()
+    return render_template('search.html', bubble=bubble, merge=merge)
+
 
 if __name__ == '__main__':
     app.run(debug=True) 
--- flask_test_app/sort_data.py
+++ flask_test_app/sort_data.py
@@ -5,15 +5,85 @@ import time
 import random
 
 
-
 def get_size():
     size = 5000000
     return size
 
+''' 
 def get_value_sought():
     global value_sought # declared global so this function only needs to be called once
     value_sought = random.randint(0,get_size())
     return value_sought
+'''
+def init():
+    global call_count 
+    call_count = 0 
+
+    start_time = 0
+    end_time = 0
+    start_time = time.time()  # Start time
+
+
+def merge_sort(arr):
+    global call_count
+    call_count += 1  # Increment the counter on each function call
+    
+    if len(arr) <= 1:
+        return arr
+
+    # Divide the array into two halves
+    mid = len(arr) // 2
+    left_half = merge_sort(arr[:mid])
+    right_half = merge_sort(arr[mid:])
+
+    # Merge the sorted halves
+    return merge(left_half, right_half)
+
+def merge(left, right):
+    sorted_array = []
+    i = j = 0
+
+    # Merge the two halves while maintaining sorted order
+    while i < len(left) and j < len(right):
+        if left[i] < right[j]:
+            sorted_array.append(left[i])
+            i += 1
+        else:
+            sorted_array.append(right[j])
+            j += 1
+
+    # Append any remaining elements
+    sorted_array.extend(left[i:])
+    sorted_array.extend(right[j:])
+
+    return sorted_array
+
+def conduct_merge_sort():
+    init()
+    sorted_arr = merge_sort(l)
+    end_time = time.time()    # End time
+    print(sorted_arr)
+
+    execution_time = end_time - start_time  # Calculate the elapsed time
+    print("Time taken to sort an unsorted array of", size, "elements took", execution_time, "seconds")
+    print("Number of function calls:", call_count)
+    #print("Sorted array:", sorted_arr)
+    
+    l = [size, value_sought, exists, call_count, execution_time]
+    return l
+    
+
+
+
+def bubble_sort():
+
+
+    #l = [size, value_sought, exists, call_count, execution_time]
+    #return l
+    return 5000
+    
+
+
 
 
 def init_data():
