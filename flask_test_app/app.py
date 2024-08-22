df7ea31515ab2cfe6f20d4b0ce529bd3426b990e
Nicholas Day <71455485+NicholasDay1992@users.noreply.github.com>
2024-08-22 17:04:25 +0100

    have linear search working on flask

--- flask_test_app/app.py
+++ flask_test_app/app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template
 #from data_processing import get_stack
 from data_set import get_data
+from search_data import init_data
 
 app = Flask(__name__)
 
@@ -11,14 +12,14 @@ def home():
     return render_template('home.html', result=csv)
  
 @app.route('/queue')
-def about():
+def queue():
     return render_template('queue.html')
 
-''' 
-@app.route('/stack')
-def about():
-    return render_template('stack.html')
-'''
+
+@app.route('/search')
+def search():
+    return_dict = init_data()
+    return render_template('search.html', result=return_dict)
 
 if __name__ == '__main__':
     app.run(debug=True) 
---
+++ flask_test_app/search_data.py
@@ -0,0 +1,57 @@
+import os
+import pandas as pd
+import numpy as np
+import time 
+import random
+
+def init_data():
+    size = 5000
+    call_count = 0 
+    l = range(size)
+
+    start_time = time.time()  # Start time
+
+    for i in l: 
+        print(i)
+        call_count += 1  
+
+    end_time = time.time()    # End time
+
+    execution_time = end_time - start_time  # Calculate the elapsed time
+    print("Time taken to read", size, "elements =", float(execution_time), "seconds")
+    print("Number of function calls:", call_count)
+    
+    l = search_value()
+    
+    #l = [size, call_count, execution_time]
+    return l
+
+
+def search_value():
+    size = 100000
+    call_count = 0 
+    value_sought = random.randint(0,size)
+    exists = False
+    l = range(size)
+
+    start_time = time.time()  # Start time
+
+    for i in l: 
+        print(i)
+        if(i == value_sought):
+            exists = True
+            call_count += 1
+            print(exists)  
+            break
+        else:
+            call_count += 1  
+
+    end_time = time.time()    # End time
+
+    execution_time = end_time - start_time  # Calculate the elapsed time
+    print("Time taken to find", value_sought, "in ", size, "elements =", float(execution_time), "seconds")
+    print("Number of function calls:", call_count)
+    
+    l = [size, value_sought, exists, call_count, execution_time]
+    return l
+
