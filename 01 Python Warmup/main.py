bfd15142c40e00a6eccd254a74af4c8f72831c3e
Nicholas Day <71455485+NicholasDay1992@users.noreply.github.com>
2024-10-07 19:37:34 +0100

    updated flask content

---
+++ 01 Python Warmup/main.py
@@ -0,0 +1,8 @@
+from Student import Student 
+
+def main():
+    nick = Student("Nick")
+    print(nick.name)
+    
+if __name__ == '__main__':
+    main()
---
+++ 01 Python Warmup/my_flask_app/data_processing.py
@@ -0,0 +1,6 @@
+
+def process_data():
+    # Simulate some data processing
+    l = [1,2,3,4,5,6,7,8,9]
+    total = sum(l)
+    return total
