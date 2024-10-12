39c2f0963b9253335d19f99935373a2a7c04e734
Nicholas Day <71455485+NicholasDay1992@users.noreply.github.com>
2024-10-12 08:40:45 +0100

    update 01 basics

--- 01 Python Warmup/Student.py
+++
@@ -1,3 +0,0 @@
-class Student:
-    def __init__(self, name):
-        self.name = name
--- 01 Python Warmup/main.py
+++
@@ -1,8 +0,0 @@
-from student import Student 
-
-def main():
-    nick = Student("Nick")
-    print(nick.name)
-
-if "__main__" == main():
-    main()
